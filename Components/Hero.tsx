9670929a7f16fefe47c0bc9d227138bcd4d66415
Bikal <bikaladhikari5@gmail.com>
2024-07-06 22:24:26 +1000

    Cv added and make available to download by clicking download

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -20,10 +20,12 @@ const Hero = () => {
             frontend and backend development, producing Quality work.
           </p>
           <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
-            <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
-              <p>Download CV</p>
-              <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
-            </button>
+            <a href="/Bikal-Adhikari-CV-Resume.pdf" download>
+              <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
+                <p>Download CV</p>
+                <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
+              </button>
+            </a>
             <a
               href="https://www.linkedin.com/in/bikal-adhikari/"
               target="_blank"
