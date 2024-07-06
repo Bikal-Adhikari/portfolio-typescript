0665d0d74d21976669b6113361ea0809b4534e2e
Bikal <bikaladhikari5@gmail.com>
2024-07-06 22:26:15 +1000

    Cv added and make available to download by clicking download

--- Components/About.tsx
+++ Components/About.tsx
@@ -26,10 +26,12 @@ const About = () => {
               position as a Software Engineer.
             </p>
           </div>
-          <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
-            <p>Download CV</p>
-            <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
-          </button>
+          <a href="/Bikal-Adhikari-CV-Resume.pdf" download>
+            <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
+              <p>Download CV</p>
+              <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
+            </button>
+          </a>
         </div>
         <div
           data-aos="fade-left"
