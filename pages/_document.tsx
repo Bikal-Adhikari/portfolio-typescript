efcb123ae64c8234e7e8d254af04b84984bfe335
Bikal <bikaladhikari5@gmail.com>
2024-07-29 21:47:26 +1000

    Integrating script to include chatbot in project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          src="https://www.chatbase.co/embed.min.js"
+          data-chatbot-id="Wub3n_l4kx3WCs0vu1B6r"
+          data-domain="www.chatbase.co"
+          defer
+        ></script>
+      </Head>
       <body className="font-primary">
         <Main />
         <NextScript />
